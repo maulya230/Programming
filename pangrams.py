421fbb7dd10eb9664e0f144f58e982ccb3411fcb
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-12-20 08:50:56 +0530

    Create pangrams.py
    A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.

---
+++ pangrams.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+import string
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    #create set for unique character
+    temp=set(s.lower())-set(' ')
+    if len(temp)==26:
+        return "pangram"
+    else:
+        return "not pangram"
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
