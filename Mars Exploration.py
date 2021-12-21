59cb19cfd0f6608bd46b5a8f3c5a0734d1e0f812
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-12-21 09:16:30 +0530

    Create Mars Exploration.py
    A space explorer's ship crashed on Mars! They send a series of SOS messages to Earth for help.
    Letters in some of the SOS messages are altered by cosmic radiation during transmission. Given the signal received by Earth as a string s, determine how many letters of the SOS message have been changed by radiation.

---
+++ Mars Exploration.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    n="SOS"*(len(s)//3)
+    count=0
+    for i in range(len(s)):
+        if s[i]!=n[i]:
+            count+=1
+    return count
+    
+      
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
