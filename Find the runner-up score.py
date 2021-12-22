c154088213093ce9447110d2bab4143f9a2f3f45
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-12-22 15:18:21 +0530

    Create Find the runner-up score.py
    Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up

---
+++ Find the runner-up score.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr=sorted(arr,reverse=True)
+    
+for i in range(n):
+    if arr[i]==arr[0]:
+        continue
+    else:
+        print(arr[i])
+        break
